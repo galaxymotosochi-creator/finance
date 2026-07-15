3c5aea085d8a663d1e1ffcaf23d6f5e1529b493a
Sergey Brin <7bokov@gmail.com>
2026-07-15 20:17:59 +0200

    fix: ширина колонки название 100% + тип nowrap

--- src/pages/finance/Categories.jsx
+++ src/pages/finance/Categories.jsx
@@ -119,8 +119,8 @@ export default function Categories() {
         <table className="data-table">
           <thead id="dirColHeaders">
             <tr>
-              <th style={{color:'#222',fontWeight:400,fontSize:'.78rem',textAlign:'left'}}>Название</th>
-              <th style={{color:'#222',fontWeight:400,fontSize:'.78rem',textAlign:'left'}}>Тип категории</th>
+              <th style={{color:'#222',fontWeight:400,fontSize:'.78rem',textAlign:'left',width:'100%'}}>Название</th>
+              <th style={{color:'#222',fontWeight:400,fontSize:'.78rem',textAlign:'left',whiteSpace:'nowrap'}}>Тип категории</th>
               <th style={{color:'#222',fontWeight:400,fontSize:'.78rem',textAlign:'center',width:'30px'}}></th>
             </tr>
           </thead>
