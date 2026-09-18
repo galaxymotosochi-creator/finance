6f48f203d3d0877db24238ba929014aa60c7d134
Sergey Brin <7bokov@gmail.com>
2026-09-18 21:18:35 +0200

    Доходы и расходы: проценты на шкалах заполняемости, как в Счетах

--- src/pages/finance/Transactions.jsx
+++ src/pages/finance/Transactions.jsx
@@ -565,7 +565,7 @@ export default function Transactions() {
                 {incomeCatsList.map((c, i) => (
                   <div key={i}>
                     <div className="tx-leg"><span className="dot" style={{background:INC_COLORS[i % INC_COLORS.length]}}></span><span className="nm">{c.name}</span><span className="amt">+{c.amount.toLocaleString()} {cur}</span></div>
-                    <div className="tx-leg-bar"><i style={{width:(incomeTotal ? c.amount / incomeTotal * 100 : 0) + '%', background:INC_COLORS[i % INC_COLORS.length]}}></i></div>
+                    <div className="tx-leg-bar"><i style={{width:(incomeTotal ? c.amount / incomeTotal * 100 : 0) + '%', background:INC_COLORS[i % INC_COLORS.length]}}>{incomeTotal ? Math.round(c.amount / incomeTotal * 100) + '%' : ''}</i></div>
                   </div>
                 ))}
               </div>
@@ -575,7 +575,7 @@ export default function Transactions() {
                 {expenseCatsList.map((c, i) => (
                   <div key={i}>
                     <div className="tx-leg"><span className="dot" style={{background:EXP_COLORS[i % EXP_COLORS.length]}}></span><span className="nm">{c.name}</span><span className="amt">−{c.amount.toLocaleString()} {cur}</span></div>
-                    <div className="tx-leg-bar"><i style={{width:(expenseTotal ? c.amount / expenseTotal * 100 : 0) + '%', background:EXP_COLORS[i % EXP_COLORS.length]}}></i></div>
+                    <div className="tx-leg-bar"><i style={{width:(expenseTotal ? c.amount / expenseTotal * 100 : 0) + '%', background:EXP_COLORS[i % EXP_COLORS.length]}}>{expenseTotal ? Math.round(c.amount / expenseTotal * 100) + '%' : ''}</i></div>
                   </div>
                 ))}
               </div>
